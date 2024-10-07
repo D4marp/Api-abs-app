a0a1f94796762ec45d7df6d9c36b60040bcd262f
Damar Galih <anahariza45.com>
2024-10-07 20:59:52 +0700

    new

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; PT.Fiyansa Mulya 2024
+  </div>
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; <div class="bullet"></div>PT.Fiyansa Mulya 2024</a>
+    </div>
+    <div class="footer-right">
+        2.3.0
+    </div>
+</footer>
